1d7c2620d87130c02255340916027808527e8d7c
goblinsan <coghlanjames@yahoo.com>
2023-08-25 22:20:59 -0400

    tests for handler

--- functions/render_trigger/src/handler.py
+++ functions/render_trigger/src/handler.py
@@ -1,3 +1,128 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:df046391e8f59d64355879a16c0a4669248f42da6c863a781bdb0b76115b5ce6
-size 3786
+import json
+import time
+import urllib.parse
+import uuid
+from types import SimpleNamespace
+from datetime import datetime
+
+import boto3
+import os
+
+S3_BUCKET = os.environ['S3_BUCKET']
+SQS_QUEUE = os.environ['SQS_QUEUE']
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+dynamo = boto3.client('dynamodb')
+sqs = boto3.client('sqs')
+
+
+def execute(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        msg_body = parse_body(response)
+        job_meta = generate_job_meta(msg_body.file_name, msg_body.output_name)
+
+        dynamo.put_item(TableName='render_jobs',
+                        Item=create_db_item(msg_body, job_meta))
+
+        put_jobs_on_queue(job_meta, msg_body)
+
+        return msg_body
+    except Exception as e:
+        print(e)
+        print(
+            'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
+                key, bucket))
+        raise e
+
+
+def put_jobs_on_queue(job_meta, msg_body):
+    entries = []
+    for i in range(msg_body.frames):
+        entry = create_sqs_entry(msg_body.file_name, i, job_meta.id_db, job_meta.full_output_path)
+        entries.append(entry)
+    max_sqs_batch_size = 10
+    response = []
+    for batch in chunks(entries, max_sqs_batch_size):
+        response.extend(sqs.send_message_batch(Entries=batch, QueueUrl=SQS_QUEUE))
+
+
+def parse_body(response):
+    msg_body = SimpleNamespace(** json.loads(response['Body'].read()))
+    return msg_body
+
+
+def generate_job_meta(file_name, output_name):
+    output_name = path_friendly_filename(output_name)
+    id_db = uuid.uuid4()
+    readable_time = get_time()
+    full_output_path = create_output_path(file_name, id_db, readable_time) + output_name
+    return SimpleNamespace(full_output_path=full_output_path, id_db=id_db, readable_time=readable_time)
+
+
+def get_time():
+    ts = time.time()
+    readable_time = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    return readable_time
+
+
+def create_sqs_entry(file_name, i, id_db, full_output_path):
+    entry = {"Id": str(i),
+             "MessageBody": create_message_body(i, full_output_path),
+             "MessageAttributes": {
+                 'RenderJobId': {
+                     'DataType': 'String',
+                     'StringValue': str(id_db)
+                 },
+                 'File': {
+                     'DataType': 'String',
+                     'StringValue': file_name
+                 },
+                 'Frame': {
+                     'DataType': 'Number',
+                     'StringValue': str(i + 1)
+                 }}
+             }
+    return entry
+
+
+def create_message_body(i, full_output_path):
+    frame_as_string = str(i + 1)
+    padded_frame_as_string = str(i + 1).zfill(5)
+    message_body = {
+        'frame': frame_as_string,
+        's3_bucket': S3_BUCKET,
+        'object_name': full_output_path + '_' + padded_frame_as_string
+    }
+    return json.dumps(message_body)
+
+
+def create_db_item(message_body, job_meta):
+    return {
+        'render_job_id': {'S': str(job_meta.id_db)},
+        'start_time': {'S': str(job_meta.readable_time)},
+        'seconds_to_expire': {'N': '300'},  # 5 minutes
+        'file_name': {'S': message_body.file_name},
+        'frames': {'N': str(message_body.frames)},
+        'output_name': {'S': job_meta.full_output_path}}
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def create_output_path(file_name, id_db, readable_time):
+    path_friendly_time = readable_time.replace(' ', '_').replace(':', '-')
+    return f"render-output/{path_friendly_filename(file_name)}/{path_friendly_time}/{str(id_db)[0:8]}/"
+
+
+def path_friendly_filename(file_name):
+    blend_extension = '.blend'
+    file_name = str.lower(file_name).split(blend_extension)[0]
+
+    return file_name.replace(' ', '_')
--- functions/render_trigger/test/test_handler.py
+++ functions/render_trigger/test/test_handler.py
@@ -1,3 +1,155 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d621041aaa47a8c30f6571b2e9bf8e8fe2dfd2809e80f4b3ce54b5d0117d538c
-size 2402
+from uuid import UUID
+
+import pytest
+from moto import mock_s3, mock_dynamodb, mock_sqs
+
+from functions.render_trigger.src.handler import *
+
+
+@pytest.fixture
+def set_envs(monkeypatch):
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "testing")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "testing")
+    monkeypatch.setenv("AWS_SECURITY_TOKEN", "testing")
+    monkeypatch.setenv("AWS_SESSION_TOKEN", "testing")
+    monkeypatch.setenv("S3_BUCKET", "EXAMPLE-BUCKET")
+    monkeypatch.setenv("SQS_QUEUE", "EXAMPLE-QUEUE")
+
+
+@pytest.fixture(scope="function")
+def s3(set_envs):
+    with mock_s3():
+        bucket_name = "EXAMPLE-BUCKET"
+        s3c = boto3.client("s3")
+        s3c.create_bucket(Bucket=bucket_name)
+        with open("resources/test-data.json", "rb") as test_file:
+            s3c.put_object(Bucket=bucket_name, Key="EXAMPLE-PREFIX/example.json", Body=test_file,
+                           ContentType="application/json")
+        yield s3c
+
+
+@pytest.fixture(scope="function")
+def sqs(set_envs):
+    with mock_sqs():
+        sqsc = boto3.client("sqs")
+        sqsc.create_queue(QueueName="EXAMPLE-QUEUE")
+        yield sqsc
+
+
+@pytest.fixture(scope="function")
+def dynamo(set_envs):
+    with mock_dynamodb():
+        dbc = boto3.client("dynamodb", region_name="us-east-1")
+        dbc.create_table(TableName='render_jobs',
+                         AttributeDefinitions=[
+                             {
+                                 "AttributeName": "render_job_id",
+                                 "AttributeType": "S"
+                             },
+                             {
+                                 "AttributeName": "start_time",
+                                 "AttributeType": "S"
+                             }
+                         ],
+                         KeySchema=[
+                             {
+                                 "AttributeName": "render_job_id",
+                                 "KeyType": "HASH"
+                             },
+                             {
+                                 "AttributeName": "start_time",
+                                 "KeyType": "RANGE"
+                             }
+                         ],
+                         ProvisionedThroughput={
+                             "ReadCapacityUnits": 1,
+                             "WriteCapacityUnits": 1
+                         }
+                         )
+        yield dbc
+
+
+@pytest.fixture
+def patch_time(monkeypatch):
+    def mock_time():
+        return 1
+
+    monkeypatch.setattr(time, 'time', mock_time)
+
+
+@pytest.fixture
+def patch_uuid(monkeypatch):
+    def mock_uuid():
+        return UUID('12345678123456781234567812345678')
+
+    monkeypatch.setattr(uuid, 'uuid4', mock_uuid)
+
+
+def test_execute(s3, dynamo, sqs, patch_time, patch_uuid):
+    with open("resources/s3-put-event.json") as file:
+        event = json.load(file)
+
+    result = execute(event, "")
+
+    get_item_response = dynamo.get_item(TableName='render_jobs',
+                                        Key={'render_job_id': {'S': '12345678-1234-5678-1234-567812345678'},
+                                             'start_time': {'S': '1970-01-01 00-00-01'}})
+
+    assert get_item_response['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert result.file_name == "test/default_cube.blend"
+    assert result.frames == 1
+    assert result.output_name == "first_render"
+
+
+def test_get_time(patch_time):
+    actual_result = get_time()
+    expected_result = "1970-01-01 00:00:01"
+    assert expected_result == actual_result
+
+
+def test_generate_job_meta(patch_time, patch_uuid):
+    actual_result = generate_job_meta("somefile", "someoutputname")
+    expected_full_output = 'render-output/somefile/1970-01-01_00-00-01/12345678/someoutputname'
+    expected_id_db = UUID('12345678-1234-5678-1234-567812345678')
+    expected_readable_time = "1970-01-01 00:00:01"
+    assert actual_result.full_output_path == expected_full_output
+    assert actual_result.id_db == expected_id_db
+    assert actual_result.readable_time == expected_readable_time
+
+
+def test_output_path():
+    file_name = "test filename.blend"
+    id_db = "c98d55ff-9880-45c2-8d1b-e34323bc6b8c"
+    readable_time = "2023-08-20 00:07:46"
+    expected_output_path = "render-output/test_filename/2023-08-20_00-07-46/c98d55ff/"
+
+    actual_output_path = create_output_path(file_name, id_db, readable_time)
+    assert expected_output_path == actual_output_path
+
+
+def test_chunk():
+    expected_chunks = [range(10, 20), range(20, 30), range(30, 35)]
+    # [[10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    # [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+    # [30, 31, 32, 33, 34]]
+
+    actual_chunks = chunks(range(10, 35), 10)
+
+    assert expected_chunks == list(actual_chunks)
+
+
+def test_path_friendly_filename():
+    filename = 'some junk.blend'
+    expected_output = 'some_junk'
+
+    assert expected_output == path_friendly_filename(filename)
+
+    filename = 'some other junk.BLEND'
+    expected_output = 'some_other_junk'
+
+    assert expected_output == path_friendly_filename(filename)
+
+    filename = 'not a blend file'
+    expected_output = 'not_a_blend_file'
+
+    assert expected_output == path_friendly_filename(filename)
